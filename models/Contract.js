07adf92c5b87d967a9513618a80c2dd9c963fa77
lacomaco <lacomaco122@gmail.com>
2019-11-12 22:54:37 +0900

    ended

--- models/Contract.js
+++ models/Contract.js
@@ -1,5 +1,8 @@
 module.exports=(sequelize,DataTypes)=>{
     return sequelize.define('contract',{
+        title:{
+            type:DataTypes.STRING(100),
+        },
         content:{
             type:DataTypes.STRING(400),
         },
